efa184f30141974044686b5a72d66d1c6ab34743
tridarma6 <triidarma06@gmail.com>
2025-03-26 11:44:05 +0800

    Create Project Bar Volume Application

---
+++ app/src/main/java/com/example/barvolume/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.barvolume
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var edtWidth: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var edtLength: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        edtWidth = findViewById(R.id.edt_weight)
+        edtHeight = findViewById(R.id.edt_height)
+        edtLength = findViewById(R.id.edt_length)
+        btnCalculate = findViewById(R.id.btn_calculate)
+        tvResult = findViewById(R.id.tv_result)
+
+        btnCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.btn_calculate){
+            val inputLength = edtLength.text.toString().trim()
+            val inputWeight = edtWidth.text.toString().trim()
+            val inputHeight = edtHeight.text.toString().trim()
+
+            val volume = inputLength.toDouble() * inputWeight.toDouble() * inputHeight.toDouble()
+            tvResult.text = volume.toString()
+        }
+    }
+}
